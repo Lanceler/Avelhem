e73b2ff2dbcc4108c87a46ff4af5cd00aea7cda7
Jose Villadolid <jvil377@wgu.edu>
2024-06-09 19:59:10 -0700

    small css change.

--- src/navigation/Navbar.jsx
+++ src/navigation/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
 
           <div>
             <div className="navLinks">
-              {<Link to="/rules">Rules</Link>}
+              {/* {<Link to="/rules">Rules</Link>} */}
               {<Link to="/demo">Demo</Link>}
               {user && <Link to="/create-game">Create Game</Link>}
               {user && <Link to="/repertoires">Repertoires</Link>}
