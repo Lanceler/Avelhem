a29b205cc2cd6fa5c6902b5a450433079aee7d03
Jose Villadolid <jvil377@wgu.edu>
2024-07-01 20:25:39 -0700

    Added loading screen.

--- src/App.jsx
+++ src/App.jsx
@@ -28,15 +28,18 @@ import LoadingImage from "./components/displays/LoadingImage";
 
 import { useCardImageSwitch } from "./hooks/useCardImageSwitch";
 
+import { AnimatePresence, motion } from "framer-motion";
+
 function App() {
   const { user, authIsReady } = useAuthContext();
 
   const [loadingImages, setLoadingImages] = useState(true);
-  const [percentLoaded, setPercentLoaded] = useState(0);
+  const [showContent, setShowContent] = useState(false);
+  const [imagesLoaded, setImagesLoaded] = useState(0);
   const { imagesLoadingList } = useCardImageSwitch();
 
-  let loadedImages = 0;
   const totalImages = imagesLoadingList.length;
+
   useEffect(() => {
     const imageElements = imagesLoadingList.map((src) => {
       const img = new Image();
@@ -46,17 +49,26 @@ function App() {
       return img;
     });
 
-    function handleImageLoad() {
-      loadedImages++;
-      if (loadedImages === totalImages) {
-        setLoadingImages(false);
-      }
-    }
-  }, [imagesLoadingList]);
+    const handleImageLoad = () => {
+      setImagesLoaded((prev) => {
+        const loaded = prev + 1;
+
+        if (loaded === totalImages) {
+          setImagesLoaded(totalImages);
+          setLoadingImages(false);
+          return loaded;
+        }
+
+        return loaded;
+      });
+    };
+  }, []);
 
   useEffect(() => {
-    setPercentLoaded(Math.round(loadedImages / totalImages) * 100);
-  }, [loadedImages]);
+    if (!loadingImages) {
+      setShowContent(true);
+    }
+  }, [loadingImages]);
 
   ////////
 
@@ -67,57 +79,79 @@ function App() {
           <BrowserRouter>
             <Navbar />
 
-            {/* <LoadingImage percentLoaded={50} /> */}
+            <div className="content">
+              {showContent && (
+                <>
+                  <Routes>
+                    <Route
+                      exact
+                      path="/"
+                      element={user ? <Home /> : <Navigate to="/login" />}
+                    />
 
-            {loadingImages && <LoadingImage percentLoaded={percentLoaded} />}
-            {!loadingImages && (
-              <>
-                <Routes>
-                  <Route
-                    exact
-                    path="/"
-                    element={user ? <Home /> : <Navigate to="/login" />}
-                  />
+                    <Route
+                      path="/login"
+                      element={!user ? <LogIn /> : <Navigate to="/" />}
+                    />
+                    <Route
+                      path="/signup"
+                      element={!user ? <SignUp /> : <Navigate to="/" />}
+                    />
 
-                  <Route
-                    path="/login"
-                    element={!user ? <LogIn /> : <Navigate to="/" />}
-                  />
-                  <Route
-                    path="/signup"
-                    element={!user ? <SignUp /> : <Navigate to="/" />}
-                  />
+                    <Route
+                      path="/repertoires"
+                      element={
+                        user ? <MyRepertoires /> : <Navigate to="/login" />
+                      }
+                    />
 
-                  <Route
-                    path="/repertoires"
-                    element={
-                      user ? <MyRepertoires /> : <Navigate to="/login" />
-                    }
-                  />
+                    <Route
+                      path="/repertoire/:id"
+                      element={user ? <Repertoire /> : <Navigate to="/login" />}
+                    />
 
-                  <Route
-                    path="/repertoire/:id"
-                    element={user ? <Repertoire /> : <Navigate to="/login" />}
-                  />
+                    <Route
+                      path="/create-game"
+                      element={user ? <CreateGame /> : <Navigate to="/login" />}
+                    />
 
-                  <Route
-                    path="/create-game"
-                    element={user ? <CreateGame /> : <Navigate to="/login" />}
-                  />
+                    <Route
+                      path="/game"
+                      element={user ? <Game /> : <Navigate to="/login" />}
+                    />
 
-                  <Route
-                    path="/game"
-                    element={user ? <Game /> : <Navigate to="/login" />}
-                  />
+                    <Route path="/rules" element={<Rules />} />
 
-                  <Route path="/rules" element={<Rules />} />
+                    <Route path="/demo/" element={<Demo />} />
 
-                  <Route path="/demo/" element={<Demo />} />
+                    <Route path="/demo/:id" element={<Demo />} />
+                  </Routes>
+                </>
+              )}
 
-                  <Route path="/demo/:id" element={<Demo />} />
-                </Routes>
-              </>
-            )}
+              <div style={{ position: "absolute", top: 0, zIndex: 1000 }}>
+                <AnimatePresence>
+                  {!showContent && (
+                    <>
+                      <motion.div
+                        layout={true}
+                        initial={{ opacity: 1, scale: 1 }}
+                        // transition={{ duration: 1.5, scale: 0.5 }}
+                        exit={{ opacity: 0, transition: { duration: 1.5 } }}
+                        className="loading-image"
+                        key={1}
+                      >
+                        <LoadingImage
+                          percentLoaded={Math.round(
+                            (imagesLoaded / totalImages) * 100
+                          )}
+                        />
+                      </motion.div>
+                    </>
+                  )}
+                </AnimatePresence>
+              </div>
+            </div>
           </BrowserRouter>
         )}
       </div>
--- src/components/displays/LoadingImage.jsx
+++ src/components/displays/LoadingImage.jsx
@@ -1,27 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./LoadingImage.css";
 
-import { AnimatePresence, motion } from "framer-motion";
-
 import LoadingBanner from "../../assets/others/LoadingBanner.png";
 
 const LoadingImage = (props) => {
   return (
     <div className="loading-image-body">
       <div className="loading-image-container">
-        <AnimatePresence>
-          <motion.div
-            transition={{ duration: 1.5, scale: 0.5 }}
-            exit={{ opacity: 0, scale: 1.5 }}
-            className="loading-image"
-            key={1}
-          >
-            <img src={LoadingBanner}></img>
-          </motion.div>
-          <div className="loading-text" key={2}>
-            {`Loading... ${props.percentLoaded}%`}
-          </div>
-        </AnimatePresence>
+        <img src={LoadingBanner}></img>
+
+        <div className="loading-text">
+          {`Loading... ${props.percentLoaded}%  `}
+        </div>
       </div>
     </div>
   );
