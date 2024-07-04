83b8f86f2db8680d1c9a484f4230f098a0263d9c
Jose Villadolid <jvil377@wgu.edu>
2024-07-03 18:18:51 -0700

    added some responsive elements

--- src/navigation/Navbar.jsx
+++ src/navigation/Navbar.jsx
@@ -20,25 +20,27 @@ const Navbar = () => {
   const closeMobileMenu = () => setClick(false);
 
   const onMouseEnter = () => {
-    if (window.innerWidth < 960) {
-      setDropdown(false);
-    } else {
-      setDropdown(true);
-    }
+    setDropdown(true);
   };
 
   const onMouseLeave = () => {
-    if (window.innerWidth < 960) {
-      setDropdown(false);
-    } else {
-      setDropdown(false);
-    }
+    setDropdown(false);
   };
 
   return (
     <>
       <div className="pushDown"></div>
       <nav className="navbar">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            className="menu-icon"
+          >
+            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+          </svg>
+        </div>
+
         <div className="leftContainer">
           <Link to="/">
             <img src={AvelhemLogo} className="avelhem-logo" />
@@ -54,7 +56,7 @@ const Navbar = () => {
               className="nav-links"
               onClick={closeMobileMenu}
             >
-              Tutorial
+              Tutorial{" "}
             </Link>
           </li>
           <li
@@ -63,7 +65,7 @@ const Navbar = () => {
             onMouseLeave={onMouseLeave}
           >
             <Link
-              to="/demo"
+              // to="/demo"
               className={`nav-links ${
                 dropdown ? "nav-links-dropExpanded" : ""
               }`}
