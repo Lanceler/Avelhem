1c104e834de478b65ba594773a62eac730986c92
Anton <avil624@wgu.com>
2024-07-20 16:10:47 -0700

    css changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,75 +19,73 @@ export default function Home() {
 
   return (
     <div className="home-body">
-      <div className="home-content">
+      <div
+        className="home-content"
+        style={{
+          backgroundImage: `url(${getBannerImage("MountainFields")})`,
+        }}
+      >
+        <div className="home-title">HOME</div>
+
         <div
-          className="home-content-background"
-          style={{
-            backgroundImage: `url(${getBannerImage("MountainFields")})`,
-          }}
+          className="home-banner"
+          style={{ backgroundImage: `url(${getBannerImage("Foreshadow")})` }}
         >
-          <div
-            className="home-banner"
-            style={{ backgroundImage: `url(${getBannerImage("Foreshadow")})` }}
-          >
-            <div className="home-banner-backdrop">
-              <div className="home-banner-title">LEARN TO PLAY</div>
-              <div className="home-banner-text-body">
-                <div className="home-banner-text">
-                  <Link to="/demo/learn">
-                    <button className="home-banner-button">
-                      Interactive Tutorial
-                    </button>
-                  </Link>
-                </div>
-                <div className="home-banner-text">
-                  <Link to="/">
-                    <button className="home-banner-button">
-                      Rulebook (To do)
-                    </button>
-                  </Link>
-                </div>
+          <div className="home-banner-backdrop">
+            <div className="home-banner-title">LEARN TO PLAY</div>
+            <div className="home-banner-text-body">
+              <div className="home-banner-text">
+                <Link to="/demo/learn">
+                  <button className="home-banner-button">
+                    Interactive Tutorial
+                  </button>
+                </Link>
+              </div>
+              <div className="home-banner-text">
+                <Link to="/">
+                  <button className="home-banner-button">
+                    Rulebook (To do)
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
+        </div>
 
-          <div
-            className="home-banner"
-            style={{ backgroundImage: `url(${getBannerImage("Challenge")})` }}
-          >
-            <div className="home-banner-backdrop">
-              <div className="home-banner-title">ENTER THE FRAY</div>
-              <div className="home-banner-text-body">
-                <div className="home-banner-text">
-                  {!user && (
-                    <Link to="/login">
-                      <button className="home-banner-button">Log In</button>
-                    </Link>
-                  )}
-                  {user && (
-                    <Link to="/create-game">
-                      <button className="home-banner-button">
-                        Host A Game
-                      </button>
-                    </Link>
-                  )}
-                </div>
-                <div className="home-banner-text">
-                  {!user && (
-                    <Link to="/SignUp">
-                      <button className="home-banner-button">
-                        Create Account
-                      </button>
-                    </Link>
-                  )}
-                  {user && (
-                    <Link to="/repertoires">
-                      <button className="home-banner-button">
-                        View Repertoires
-                      </button>
-                    </Link>
-                  )}
-                </div>
+        <div
+          className="home-banner"
+          style={{ backgroundImage: `url(${getBannerImage("Challenge")})` }}
+        >
+          <div className="home-banner-backdrop">
+            <div className="home-banner-title">ENTER THE FRAY</div>
+            <div className="home-banner-text-body">
+              <div className="home-banner-text">
+                {!user && (
+                  <Link to="/login">
+                    <button className="home-banner-button">Log In</button>
+                  </Link>
+                )}
+                {user && (
+                  <Link to="/create-game">
+                    <button className="home-banner-button">Host A Game</button>
+                  </Link>
+                )}
+              </div>
+              <div className="home-banner-text">
+                {!user && (
+                  <Link to="/SignUp">
+                    <button className="home-banner-button">
+                      Create Account
+                    </button>
+                  </Link>
+                )}
+                {user && (
+                  <Link to="/repertoires">
+                    <button className="home-banner-button">
+                      View Repertoires
+                    </button>
+                  </Link>
+                )}
               </div>
             </div>
           </div>
