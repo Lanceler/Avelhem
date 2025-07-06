c04e5e3c0ca0f5e9aff77b93cb7e9a5541c7453a
Joel <joel.villadolig@gmail.com>
2025-07-06 16:10:37 -0700

    added dice glow

--- src/components/BoardArea.jsx
+++ src/components/BoardArea.jsx
@@ -21,8 +21,6 @@ import PlayerSkillHand from "./hand/PlayerSkillHand";
 import SkillHandBack from "./hand/SkillHandBack";
 import AvelhemHandBack from "./hand/AvelhemHandBack";
 
-import SovereignTactics from "./displays/SovereignTactics";
-
 import UnitInfo from "./modals/UnitInfo";
 import ViewBPUpgrades from "./modals/ViewBPUpgrades";
 import InfoPopUp from "./modals/InfoPopUp";
@@ -57,6 +55,7 @@ const BoardArea = (props) => {
   const { demoGuide } = useSelector((state) => state.demoGuide);
   const { demoCount } = useSelector((state) => state.demoCount);
   const { magnifiedSkill } = useSelector((state) => state.magnifiedSkill);
+  const { getTacticImage } = useGetImages();
 
   const [zones, setZones] = useState(null);
   const [validZones, setValidZones] = useState([]);
@@ -116,6 +115,11 @@ const BoardArea = (props) => {
   const [enemyBPFlash, setEnemyBPFlash] = useState(false);
   const [selfBP, setSelfBP] = useState(0);
   const [selfBPFlash, setSelfBPFlash] = useState(false);
+
+  const [die1, setDie1] = useState(null);
+  const [die2, setDie2] = useState(null);
+  const [die1Flash, setDie1Flash] = useState(false);
+  const [die2Flash, setDie2Flash] = useState(false);
 
   useEffect(() => {
     const imageElements = imagesLoadingList.map((src) => {
@@ -206,6 +210,12 @@ const BoardArea = (props) => {
       setSelfDP(localGameState[self].defiancePoints);
       setEnemyBP(localGameState[enemy].bountyPoints);
       setSelfBP(localGameState[self].bountyPoints);
+      setDie1(
+        localGameState?.tactics[0]?.stock + localGameState?.tactics[0]?.face
+      );
+      setDie2(
+        localGameState?.tactics[1]?.stock + localGameState?.tactics[1]?.face
+      );
     }
 
     // if (localGameState) {
@@ -236,6 +246,18 @@ const BoardArea = (props) => {
     const timeout = setTimeout(() => setSelfBPFlash(false), 1350);
     return () => clearTimeout(timeout);
   }, [selfBP]);
+
+  useEffect(() => {
+    setDie1Flash(true);
+    const timeout = setTimeout(() => setDie1Flash(false), 1550);
+    return () => clearTimeout(timeout);
+  }, [die1]);
+
+  useEffect(() => {
+    setDie2Flash(true);
+    const timeout = setTimeout(() => setDie2Flash(false), 1550);
+    return () => clearTimeout(timeout);
+  }, [die2]);
 
   useEffect(() => {
     if (demoGuide === "Learn-overview") {
@@ -487,10 +509,16 @@ const BoardArea = (props) => {
 
   //=========================
 
-  const canClick = (element1) => {
+  const canClick = (element1, element2) => {
     switch (demoGuide) {
       case "Learn-overview":
         switch (demoCount) {
+          case 38:
+            return element2 === 0;
+          case 44:
+            return element2 === 1;
+          case 63:
+            return element1 === "?";
           case 120:
             return element1 === "End Button";
           //////////////////
@@ -502,6 +530,9 @@ const BoardArea = (props) => {
     switch (demoGuide) {
       case "Learn-overview":
         switch (demoCount) {
+          case 38:
+          case 44:
+          case 63:
           case 120:
             dispatch(updateDemoCount(demoCount + 1));
             break;
@@ -578,7 +609,8 @@ const BoardArea = (props) => {
             pointerEvents: props.userRole === "spectator" ? "none" : "",
           }}
         >
-          <SovereignTactics userRole={props.userRole} />
+          {sovereignTactics()}
+
           {props.userRole === "spectator" && (
             <>
               <div className="board-spectating-label">
@@ -663,6 +695,85 @@ const BoardArea = (props) => {
             </div>
           </div>
         </div>
+      </div>
+    );
+  };
+
+  const sovereignTactics = () => {
+    const handleClick = (face, dice) => {
+      if (
+        localGameState.turnPlayer === self &&
+        localGameState.currentResolution[
+          localGameState.currentResolution.length - 1
+        ].resolution === "Execution Phase"
+      ) {
+        let newGameState = JSON.parse(JSON.stringify(localGameState));
+
+        newGameState.currentResolution.push({
+          resolution: "Misc.",
+          resolution2: "Selecting Tactical Action - Sovereign",
+          dice: dice,
+          face: face,
+        });
+
+        dispatch(updateState(newGameState));
+      }
+    };
+
+    const handleInfoClick = () => {
+      setInfoPopUp("Tactics");
+    };
+
+    return (
+      <div className="tacticSovereignContainer">
+        {infoPopUp && (
+          <InfoPopUp info={infoPopUp} setInfoPopUp={setInfoPopUp} />
+        )}
+        <div className="tacticSovereignContainer-info">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            className={`question-icon ${canClick("?") ? "demoClick" : ""}`}
+            onClick={() => {
+              handleInfoClick();
+              handleUpdateDemoGuide();
+            }}
+            style={{
+              pointerEvents: props.userRole === "spectator" ? "all" : "",
+            }}
+          >
+            <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM169.8 165.3c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
+          </svg>
+        </div>
+
+        {localGameState.tactics.map((tactic, index) => (
+          <div className={`tacticSovereignDice}`} key={index}>
+            <div
+              className={` tacticSovereignBG ${
+                tactic.stock < 1 || localGameState.turnPlayer === enemy
+                  ? "disabledtacticSovereignBG"
+                  : ""
+              } ${canClick("Tactic", index) ? "demoClick" : ""}
+              ${[die1Flash, die2Flash][index] ? "diceFlash" : ""}
+              `}
+              onClick={() => {
+                handleClick(tactic.face, index);
+                handleUpdateDemoGuide();
+              }}
+            >
+              <div
+                key={index}
+                className="tacticSovereign"
+                style={{
+                  backgroundImage: `url(${getTacticImage(tactic.face)})`,
+                }}
+              ></div>
+            </div>
+            <h2 className="tacticSovereignLabel">
+              {tactic.face} ({tactic.stock})
+            </h2>
+          </div>
+        ))}
       </div>
     );
   };
